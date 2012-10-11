0542abc1c0e620624abc522b70376ccc8fb5ac19
suehring <suehring@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2012-10-11 07:22:52 +0000

    remove VPS_SYNTAX_CHANGES also in TypeDef.h

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -45,7 +45,6 @@
 #define MAX_CPB_CNT                     32  ///< Upper bound of (cpb_cnt_minus1 + 1)
 #define MAX_NUM_LAYER_IDS                64
 
-#define VPS_SYNTAX_CHANGES               1  ///< J0550/J0562: changes to VPS syntax
 #define PROFILE_TIER_LEVEL_SYNTAX        1  ///< J0562: Include profile_tier_level() syntax structure; not used for now
 #define SAO_LUM_CHROMA_ONOFF_FLAGS       1  ///< J0087: slice-level independent luma/chroma SAO on/off flag 
 #define LTRP_IN_SPS                      1  ///< J0116: Include support for signalling LTRP LSBs in the SPS, and index them in the slice header.
